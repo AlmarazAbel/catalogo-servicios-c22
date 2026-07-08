bd455c0aa9f87236292abdd7e860317fd9f46d14
Abel Almaraz <almarazabeljesus@gmail.com>
2026-07-08 00:28:35 -0300

    agregamos la queri listarServiciosApi en el componente Administrador e inicio

--- src/components/pages/Inicio.tsx
+++ src/components/pages/Inicio.tsx
@@ -1,8 +1,27 @@
-import CardServicio from "../services/CardServicio";
-import { useAppContext } from "../../context/AppContext";
-const Inicio = () => {
-  const { servicios } = useAppContext();
 
+import CardServicio from "../services/CardServicio";
+//import { useAppContext } from "../../context/AppContext";
+import type { Servicio } from "../../interfaces/servicios";
+import {useEffect, useState } from "react";
+import { listarServiciosApi } from "../../helpers/queries";
+const Inicio = () => {
+ // const { servicios } = useAppContext();
+ 
+ const [servicios, setServicios] = useState<Servicio[]>([])
+
+ useEffect(()=>{
+  cargarServicios();
+ },[])
+
+ const cargarServicios = async()=>{
+const respuestaServicios = await listarServiciosApi()
+if(respuestaServicios && respuestaServicios.status=== 200)
+{
+  const datos= await respuestaServicios.json()
+  console.log(datos)
+  setServicios(datos)
+}
+ }
   return (
     <section className="space-y-8 animate-fadeIn">
       {/* Encabezado con estilo moderno */}
@@ -24,7 +43,7 @@ const Inicio = () => {
       {servicios.length > 0 ? (
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {servicios.map((servicio) => (
-            <CardServicio key={servicio.id} servicio={servicio} />
+            <CardServicio key={servicio._id} servicio={servicio} />
           ))}
         </div>
       ) : (
